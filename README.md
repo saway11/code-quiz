de6d4d7d06e6e6dcab4ec9e7e20b07c06265a6cd
unknown <rgarcia20.m@gmail.com>
2023-04-23 16:29:02 -0700

    correct js, updated readme, added comments

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# code-quiz
+# Trivia Quiz
+
+# URL
+
+# Description
+This quiz created on the JavaScript fundamentals that stores high scores
+so that the user can guage their progress compared to other users
+
+# Usage
+The user's initial view of the quiz will consist of reading the instructions, have access to the "view high score", "go back" and "clear high scores" button. As soon as the user activates the "start quiz" button questions and answers will appear. The user will have 30 seconds to answer each question. If the user runs out of time a different screen will appear which will display "complete". The user must enter their initals in order to receive their final score. The user has the ability to go back to the main page. If the user completes the quiz they will receive a final score. The user has the ability to view their high scores or clear their high scores.
+
+# Screenshot
+
+
+# Credits
+UC Berkeley Coding Bootcamp, Google, W3 School, ChatGBT
+
+# License
+MIT License
+
+Copyright (c) 2023 Rosa Garcia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documention files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or subtantial portion of the Software.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,168 +1,319 @@
 var containerQuestionEl = document.getElementById("question-container");
-var containerStartEl = document.getElementById("started-container");
-var containerEndEl = document.getElementById("end-container");
-var containerScoreE1 = document.getElementById("score-banner");
-var formInitials = document.getElementById("initials-form");
-var containerHighScoresE1 = document.getElementById("high-score-container");
-var viewHighScoreEl = document.getElementById("view-high-scores");
-var listHighScoreEl = document.getElementById("high-score-list");
-var correctEl = document.getElementById("correct");
-var wrongEl = document.getElementById("wrong");
-
+var containerStartEl = document.getElementById("starter-container");
+var containerEndEl = document.getElementById("end-container")
+var containerScoreEl = document.getElementById("score-banner")
+var formInitials = document.getElementById("initials-form")
+var containerHighScoresEl = document.getElementById("high-score-container")
+var ViewHighScoreEl = document.getElementById("view-high-scores")
+var listHighScoreEl = document.getElementById("high-score-list")
+var correctEl = document.getElementById("correct")
+var wrongEl = document.getElementById("wrong")
+//buttons
 var btnStartEl = document.querySelector("#start-game");
-var btnGoBackEl = document.querySelector("#go-back");
-var btnClearScores = document.querySelector("#clear-high-scores");
-
-var questionEl = document.getElementById("question");
-var answerbuttonsEl = document.getElementById("answer-buttons");
+var btnGoBackEl = document.querySelector("#go-back")
+var btnClearScoresEl = document.querySelector("#clear-high-scores")
+//questions/answers element
+var questionEl = document.getElementById("question")
+var answerbuttonsEl = document.getElementById("answer-buttons")
 var timerEl = document.querySelector("#timer");
 var score = 0;
 var timeleft;
 var gameover
 timerEl.innerText = 0;
 
-varHighScores = [];
+//High Score Array
+var HighScores = [];
 
+ //assign array details for questions 
 var arrayShuffledQuestions
-var QueationIndex = 0;
+var QuestionIndex = 0
 
+
+
+// The array of questions for our quiz game.
 var questions = [
-    {
-        q: 'How many times a year does the sun set and rise in the Arctic?',
-        a: 'One time',
-        choices: [{choice: '1. two times'}, {choice: '2. one time'}, {choice: '3. four times'}, {
-         choice: '4. three times'}]
-    },
-    {
-        q: 'What day of the week is the least  amount of babies are born?',
-        a: 'Sunday',
-        choices: [{choice: '1. Monday'}, {choice: '2. Saturday'}, {choice: '3. Sunday'}, {
-         choice: '4. Tuesday'}]
-    },
-    {
-        q: 'What is a Golden Birthday?',
-        a: 'Turning the same age as the date of birth',
-        choices: [{choice: '1. turning the same age as the date of birth'}, {choice: '2. 25th Birthday'}, {choice: '3. 30th Birthday'}, {
-         choice: '4. 15 Birthday'}]
-    },
-    {
-        q: 'How many different varieties of watermelon are there?',
-        a: 'More than 300',
-        choices: [{choice: '1. More than 30'}, {choice: '2. More than 300'}, {choice: '3. More than 100'}, {
-         choice: '4. More than 50'}]
-    },
+  { q: 'What day of the week has the least amount of babies born?', 
+    a: '2. Sunday', 
+    choices: [{choice: '1. Monday'}, {choice: '2. Sunday'}, {choice: '3. Wednesday'}, {choice: '4. Saturday'}]
+  },
+  { q: 'What is a Golden Birthday?', 
+    a: '3. Turning the same age as the date of birth', 
+    choices: [{choice: '1. 25th Birthday'}, {choice: '2. 15th Birthday'}, {choice: '3. Turning the same age as the date of birth'}, {choice: '4. 30th Birthday'}]
+  },
+  { q: 'How tall was the largest snowman?', 
+    a: '1. 122 feet 1 inch', 
+    choices: [{choice: '1. 122 feet 1 inch'}, {choice: '2. 100 feet 10 inches'}, {choice: '3. 110 feet 3 inches'}, {choice: '4. 120 feet 3 inches'}]
+  },
+  { q: 'Snowflakes usually have how many sides?', 
+    a: '4. Six', 
+    choices: [{choice: '1. Eight'}, {choice: '2. Three'}, {choice: '3. Four'}, {choice: '4. Six'}]
+  },
+  { q: 'What is another name for Superman', 
+    a: '1. The Man of Steel', 
+    choices: [{choice: '1. The Man of Steel'}, {choice: '2. The Caped Crusader'}, {choice: '3. The Masked Avenger'}, {choice: '4. The Caped Man'}]
+  },
+  { q: 'What is Iron Mans real name?', 
+    a: '2. Tony Stark', 
+    choices: [{choice: '1. Tony Star'}, {choice: '2. Tony Stark'}, {choice: '3. Robert Downey Jr.'}, {choice: '4. Robert Downey'}]
+  },
+  { q: 'Wonder Woman is a superhero from what comic book publisher?', 
+    a: '2. DC Comics', 
+    choices: [{choice: '1. Marvel Comics'}, {choice: '2. DC Comics'}, {choice: '3. Image Comics'}, {choice: '4. al of the above'}]
+  },
 ];
 
+  //if go back button is hit on high score page
 var renderStartPage = function () {
-    containerHighScoresEl.classList.add("hide")
-    containerHighScoresEl.classList.remove("show")
-    containerStartEl.classList.remove("hide")
-    containerStartEl.classList.add("show")
-    containerScoresEl.removeChild(containerScoreEl.lastChild)
-    QuestionIndex = 0
-    gameover = ""
-    timerEl.textContent = 0
-    score = 0
+  containerHighScoresEl.classList.add("hide")
+  containerHighScoresEl.classList.remove("show")
+  containerStartEl.classList.remove("hide")
+  containerStartEl.classList.add("show")
+  containerScoreEl.removeChild(containerScoreEl.lastChild)
+  QuestionIndex = 0
+  gameover = ""
+  timerEl.textContent = 0 
+  score = 0
 
-    if (correctEl.className = "show") {
-        correctEl.classList.remove("show");
-        correctEl.classList.add("hide");
-    }
-    if (wrongEl.className = "show") {
-        wrongEl.classList.remove("show");
-        wrongEl.classList.add("hide");
-    }
+  if (correctEl.className = "show") {
+      correctEl.classList.remove("show");
+      correctEl.classList.add("hide")
+  }
+  if (wrongEl.className = "show") {
+      wrongEl.classList.remove("show");
+      wrongEl.classList.add("hide");
+  }
 }
 
+//every second, check if game-over is true, or if there is time left. Start time at 30. 
 var setTime = function () {
-    timeleft = 30;
+  timeleft = 30;
 
-    var timercheck = setInterval(function() {
-        timerEl.innerText = timeleft;
-        timeleft--
+var timercheck = setInterval(function() {
+  timerEl.innerText = timeleft;
+  timeleft--
 
-        if (gameover) {
-            clearInterval(timercheck)
-        }
+  if (gameover) {
+      clearInterval(timercheck)
+  }
+ 
+  if (timeleft < 0) {
+      showScore()
+      timerEl.innerText = 0
+      clearInterval(timercheck)
+  }
 
-        if (timeLeft < 0) {
-            showScore()
-            timerEl.innerText = 0
-            clearInterval(timercheck)
-        }
-    }, 1000)
+  }, 1000)
 }
 
 var startGame = function() {
-    containerStartEl.classList.add('hide');
-    containerStartEl.classList.remove('show');
-    containerQuestionEl.classList.remove('hide');
-    containerQuestionEl.classList.add('show');
-
-    arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5)
-    setTime()
-    setQuestion()
+  //add classes to show/hide start and quiz screen
+  containerStartEl.classList.add('hide');
+  containerStartEl.classList.remove('show');
+  containerQuestionEl.classList.remove('hide');
+  containerQuestionEl.classList.add('show');
+  //Shuffle the questions so they show in random order
+  arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5)
+  setTime()
+  setQuestion()
 }
 
+//set next question for quiz
 var setQuestion = function() {
-    resetAnswers()
-    displayQuestion(arrayShuffledQuestions[QuestionIndex])
+  resetAnswers()
+  displayQuestion(arrayShuffledQuestions[QuestionIndex])
 }
 
+//remove answer buttons
 var resetAnswers = function() {
-    while (answerbuttonsEl.firstChild) {
-        answerbuttonsEl.removeChild(answerbuttonsEl.firstChild)
-    };
+  while (answerbuttonsEl.firstChild) {
+      answerbuttonsEl.removeChild(answerbuttonsEl.firstChild)
+  };
 };
 
-var displayQuestions = function(index) {
-    questionEl.innerText = index.q
-    for (var i = 0; i < index.choices.length; i++) {
-        var answerbutton = document.createElement('button')
-        answerbutton.innerText = index.choices[i].choice
-        answerbutton.classList.add('btn')
-        answerbutton.classList.add('answerbtn')
-        answerbutton.addEventListener("click", answerCheck)
-        answerbuttonsEl.appendChild(answerbutton)
-    }
-};
-
+//display question information (including answer buttons)
+var displayQuestion = function(index) {
+  questionEl.innerText = index.q
+  for (var i = 0; i < index.choices.length; i++) {
+      var answerbutton = document.createElement('button')
+      answerbutton.innerText = index.choices[i].choice
+      answerbutton.classList.add('btn')
+      answerbutton.classList.add('answerbtn')
+      answerbutton.addEventListener("click", answerCheck)
+      answerbuttonsEl.appendChild(answerbutton)
+      }
+  };
+//display correct! on screen
 var answerCorrect = function() {
-    if (correctEl.className = "hide") {
-        correctEl.classList.remove("hide")
-        correctEl.classList.add("banner")
-        wrongEl.classList.remove("banner")
-        wrongEl.classList.add("hide")
-        }
-    }  
-
+  if (correctEl.className = "hide") {
+      correctEl.classList.remove("hide")
+      correctEl.classList.add("banner")
+      wrongEl.classList.remove("banner")
+      wrongEl.classList.add("hide")
+      }
+  }  
+//display wrong! on screen
 var answerWrong = function() {
-    if (wrongEl.className = "hide") {
-        wrongEl.classList.remove("hide")
-        wrongEl.classList.add("banner")
-        correctEl.classList.remove("banner")
-        correctEl.classList.add("hide")
-    }
+  if (wrongEl.className = "hide") {
+      wrongEl.classList.remove("hide")
+      wrongEl.classList.add("banner")
+      correctEl.classList.remove("banner")
+      correctEl.classList.add("hide")
+  }
 }
 
+//check if answer is correct    
 var answerCheck = function(event) {
-    var selectednaswer = event.target
-    if (arrayShuffledQuestions[QuestionIndex].a === selectedanswer.innerText) {
-        answerCorrect()
-        score = score + 7
-    }
+  var selectedanswer = event.target
+      if (arrayShuffledQuestions[QuestionIndex].a === selectedanswer.innerText){
+          answerCorrect()
+          score = score + 7
+      }
 
-    else {
+      else {
         answerWrong()
-        score = score -1;
+        score = score - 1;
         timeleft = timeleft - 3;
     };
+
+  //go to next question, check if there is more questions
     QuestionIndex++
-    if  (arrayShuffledQuestions.length > QuestionIndex + 1) {
-        setQuestion()
-    }   
-    else {
-       gameover = "true";
-       showScore();
-        }
+      if  (arrayShuffledQuestions.length > QuestionIndex + 1) {
+          setQuestion()
+      }   
+      else {
+         gameover = "true";
+         showScore();
+          }
 }
+
+  //Display total score screen at end of game
+var showScore = function () {
+  containerQuestionEl.classList.add("hide");
+  containerEndEl.classList.remove("hide");
+  containerEndEl.classList.add("show");
+
+  var scoreDisplay = document.createElement("p");
+  scoreDisplay.innerText = ("Your final score is " + score + "!");
+  containerScoreEl.appendChild(scoreDisplay);
+}       
+
+//create high score values
+var createHighScore = function(event) { 
+  event.preventDefault() 
+  var initials = document.querySelector("#initials").value;
+  if (!initials) {
+    alert("Enter your intials!");
+    return;
+  }
+
+formInitials.reset();
+
+var HighScore = {
+initials: initials,
+score: score
+} 
+
+//push and sort scores
+HighScores.push(HighScore);
+HighScores.sort((a, b) => {return b.score-a.score});
+
+//clear visibile list to resort
+while (listHighScoreEl.firstChild) {
+ listHighScoreEl.removeChild(listHighScoreEl.firstChild)
+}
+//create elements in order of high scores
+for (var i = 0; i < HighScores.length; i++) {
+var highscoreEl = document.createElement("li");
+highscoreEl.ClassName = "high-score";
+highscoreEl.innerHTML = HighScores[i].initials + " - " + HighScores[i].score;
+listHighScoreEl.appendChild(highscoreEl);
+}
+
+saveHighScore();
+displayHighScores();
+
+}
+//save high score
+var saveHighScore = function () {
+  localStorage.setItem("HighScores", JSON.stringify(HighScores))
+      
+}
+
+//load values/ called on page load
+var loadHighScore = function () {
+  var LoadedHighScores = localStorage.getItem("HighScores")
+      if (!LoadedHighScores) {
+      return false;
+  }
+
+  LoadedHighScores = JSON.parse(LoadedHighScores);
+  LoadedHighScores.sort((a, b) => {return b.score-a.score})
+
+
+  for (var i = 0; i < LoadedHighScores.length; i++) {
+      var highscoreEl = document.createElement("li");
+      highscoreEl.ClassName = "high-score";
+      highscoreEl.innerText = LoadedHighScores[i].initials + " - " + LoadedHighScores[i].score;
+      listHighScoreEl.appendChild(highscoreEl);
+
+      HighScores.push(LoadedHighScores[i]);
+      
+  }
+}  
+
+//display high score screen from link or when intiials entered
+var displayHighScores = function() {
+
+  containerHighScoresEl.classList.remove("hide");
+  containerHighScoresEl.classList.add("show");
+  gameover = "true"
+
+  if (containerEndEl.className = "show") {
+      containerEndEl.classList.remove("show");
+      containerEndEl.classList.add("hide");
+      }
+  if (containerStartEl.className = "show") {
+      containerStartEl.classList.remove("show");
+      containerStartEl.classList.add("hide");
+      }
+      
+  if (containerQuestionEl.className = "show") {
+      containerQuestionEl.classList.remove("show");
+      containerQuestionEl.classList.add("hide");
+      }
+
+  if (correctEl.className = "show") {
+      correctEl.classList.remove("show");
+      correctEl.classList.add("hide");
+  }
+
+  if (wrongEl.className = "show") {
+      wrongEl.classList.remove("show");
+      wrongEl.classList.add("hide");
+      }
+  
+}
+//clears high scores
+var clearScores = function () {
+  HighScores = [];
+
+  while (listHighScoreEl.firstChild) {
+      listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
+
+  localStorage.clear(HighScores);
+
+} 
+
+loadHighScore()
+  
+//on start click, start game
+btnStartEl.addEventListener("click", startGame)
+//on submit button -- enter or click
+formInitials.addEventListener("submit", createHighScore)
+//when view high-scores is clicked
+ViewHighScoreEl.addEventListener("click", displayHighScores)
+//Go back button
+btnGoBackEl.addEventListener("click", renderStartPage)
+//clear scores button
+btnClearScoresEl.addEventListener("click", clearScores)
